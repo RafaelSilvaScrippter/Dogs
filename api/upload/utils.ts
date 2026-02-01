25ceaee0ea7f0c58d97ec8fbca68180f461dc3f7
Rafael Silva <nativaeleven@gmail.com>
2026-02-01 09:22:51 -0300

    Controlando o limite de bytes pra postar foto

---
+++ api/upload/utils.ts
@@ -0,0 +1,17 @@
+import { Transform } from "node:stream"
+import { RouterError } from "../../core/utils/router-error.ts"
+
+export function LimitBites(max:number){
+    let size = 0
+
+    return new Transform({
+        transform(chunk,_enc,next){
+            size +=chunk.length
+
+            if(size > max){
+                return next(new RouterError(413,'corpo muito gramge'))
+            }
+            next(null,chunk)
+        }
+    })
+}
