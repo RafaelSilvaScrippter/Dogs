de3cd39bf720687862c58cd55ac29a96c135a33c
Rafael Silva <nativaeleven@gmail.com>
2026-02-03 15:39:34 -0300

    Upload files

--- api/upload/utils.ts
+++ api/upload/utils.ts
@@ -2,8 +2,8 @@ import { Transform } from "node:stream"
 import { RouterError } from "../../core/utils/router-error.ts"
 
 export function LimitBites(max:number){
+    
     let size = 0
-
     return new Transform({
         transform(chunk,_enc,next){
             size +=chunk.length
