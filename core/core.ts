395d8d70d1f0d70a6727acd9a2f6f97b79598b87
Rafael Silva <nativaeleven@gmail.com>
2026-02-12 14:32:24 -0300

    Origin mudada

--- core/core.ts
+++ core/core.ts
@@ -20,7 +20,7 @@ export class Core{
     handler = async (request:IncomingMessage,response:ServerResponse) =>{
         const req = await customRequest(request)
         const res = customResponse(response)
-            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
               res.setHeader("Access-Control-Allow-Credentials", "true");
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
